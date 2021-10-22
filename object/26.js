3c6b52dc2577d6d9c00c489ec325efc62a8c9831
panyue <90938238g@gmail.com>
2021-10-22 13:24:57 +0800

    json转化为js可操作类型

---
+++ object/26.js
@@ -0,0 +1,22 @@
+//json转化为js可操作类型
+
+let h = {
+    title: 'learning js', 
+    url: 'www.learning-js.com',
+    teacher: {
+        name: 'py-test'
+    },
+};
+
+
+//压缩为json字符串格式
+let json = JSON.stringify(h, null, 2);
+console.log(json); //json格式
+
+//解析为js可操纵对象
+let obj = JSON.parse(json, (key, value) => {
+    // console.log(`${key}, ${value}`);
+    if(key === 'title') value = `happy ${value}`;
+    return value
+});
+console.log(obj.title); 
