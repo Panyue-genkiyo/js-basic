72f825329b56792df17c2c7f2d1e07c9526f4864
panyue <90938238g@gmail.com>
2021-10-27 11:06:14 +0800

    /*面向对象多态*/

---
+++ 原型与继承/26.js
@@ -0,0 +1,43 @@
+/*面向对象多态*/
+
+function User(){
+    
+}
+
+User.prototype.show = function(){
+    //这个this因为是动态的其实就有多态哪味了
+    console.log(this.description());
+}
+
+function Admin(){
+    
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.description = function(){
+    return '管理员在此';
+}
+
+function Member(){
+    
+}
+
+Member.prototype = Object.create(User.prototype);
+Member.prototype.description = function(){
+    return '我是会员'
+}
+
+function Enterprise(){
+    
+}
+
+Enterprise.prototype = Object.create(User.prototype);
+Enterprise.prototype.description = function(){
+    return '企业账户'
+}
+
+for(const obj of [ new Admin(), new Member(), new Enterprise() ]){
+        /*多态*/
+        obj.show();
+}
