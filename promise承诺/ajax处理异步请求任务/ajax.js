5fc93e24a9b665a7a8eae45f9963913f7c6606aa
panyue <90938238g@gmail.com>
2021-11-03 17:16:11 +0800

    promise封装ajax请求

--- promise承诺/ajax处理异步请求任务/ajax.js
+++ promise承诺/ajax处理异步请求任务/ajax.js
@@ -1,13 +1,18 @@
-function ajax(url, callback) {
-    // 创建XMLHttpRequest对象
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onload = function () {
-        if(this.status === 200){
-            callback(JSON.parse(this.response));
-        }else{
-            console.log('加载失败');
+function ajax(url) {
+    return new Promise(function(resolve, reject) {
+        // 创建XMLHttpRequest对象
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.onload = function () {
+           if(this.status === 200){
+               resolve(JSON.parse(this.response));
+           }else{
+               reject('加载失败');
+           }
         }
-    }
+        xhr.onerror = function(){
+            reject(this);
+        }
+    });
 }
