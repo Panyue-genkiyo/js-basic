28af9d200ff90fb73f28c7a3689939361163721d
panyue <90938238g@gmail.com>
2021-11-03 20:26:44 +0800

    增加:加载时弹出loading,,,

--- promise承诺/ajax处理异步请求任务/ajax.js
+++ promise承诺/ajax处理异步请求任务/ajax.js
@@ -14,6 +14,9 @@ class HttpError extends Error {
 
 function ajax(url) {
     return new Promise(function(resolve, reject) {
+     
+        py.style.display = 'block'; //在发送请求时弹出loading框
+
         // 创建XMLHttpRequest对象
         if(!/^https?/i.test(url)){
             throw new ParamError('URL必须以http或https开头');
@@ -23,11 +26,15 @@ function ajax(url) {
         xhr.send();
         xhr.onload = function () {
            if(this.status === 200){
-               resolve(JSON.parse(this.response));
+               setTimeout(() => {
+                     resolve(JSON.parse(this.response));
+               }, 2000);
            }else if(this.status === 404){
             //    reject('加载失败');
             //  throw new HttpError('加载失败'); //这里抛出错误是不行的，因为这个错误是异步的，而不是同步执行的,在异步的回调函数中抛出错误无法被外界的promise的catch到
-             reject(new HttpError('查询的用户不存在'));
+              setTimeout(() => {
+                   reject(new HttpError('查询的用户不存在'));
+              });
            }
         }
         xhr.onerror = function(){
