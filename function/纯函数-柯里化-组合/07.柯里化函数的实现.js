22ec5ed10070e9c6f8691a6129dd99bbcf967d81
panyue <90938238g@gmail.com>
2021-11-09 20:28:24 +0800

    递归实现函数柯里化函数

--- function/纯函数-柯里化-组合/07.柯里化函数的实现.js
+++ function/纯函数-柯里化-组合/07.柯里化函数的实现.js
@@ -5,6 +5,7 @@ function currying(fn){
     return function curried(...args){
         //判断已经接受到参数的个数是否等于函数的参数个数
         //如果大于或等于原来函数接受的参数的个数，则直接执行函数
+        //这里需要绑定this，因为也有可能外界参数拿这个返回的函数去显示绑定this来调用(apply,call);
         if(args.length >= fn.length) return fn.call(this, ...args);
         else{
             //没有达到个数时,需要返回一个新的函数，继续来接受参数
