bfb6f5a5412c92b3c52946eb34169ace4b128663
panyue <90938238g@gmail.com>
2021-11-09 09:52:14 +0800

    修复对this值判断，防止falsy类值(''或0)当做this时，this被绑定到window

--- function/实现apply,call,bind/02.apply函数的实现.js
+++ function/实现apply,call,bind/02.apply函数的实现.js
@@ -3,7 +3,7 @@ Function.prototype.myApply = function(thisArg, pramsArray) {
    //获取要执行的函数
    let fn = this;
    //判断this是不是非对象类型或者undefined和null 在node环境下全局为global
-   thisArg = thisArg ? Object(thisArg) : typeof window === 'undefined' ? global : window;
+   thisArg = ( this !== null && this !== undefined ) ? Object(thisArg) : typeof window === 'undefined' ? global : window;
    //把该函数加入该对象作为隐式绑定 绑定该函数的this为thisArg
    thisArg.fn = fn;
    //调用该函数  //防止原函数没有参数，导致展开undefined或者null出错，我们给其加了一个默认的值[]
@@ -23,7 +23,9 @@ foo.myApply(null, [1, 2]);
 
 
 function bar(){
-    console.log('hello,我是没有参数')
+    console.log('hello,我是没有参数', this)
 }
 
 bar.myApply(null);
+
+bar.myApply('', [3,4]);
