41efebc106468865931d115fb89143f2ee4e2a75
panyue <90938238g@gmail.com>
2021-11-09 09:50:58 +0800

    修复对this值判断，防止falsy类值(''或0)当做this时，this被绑定到window

--- function/实现apply,call,bind/01.call函数的实现.js
+++ function/实现apply,call,bind/01.call函数的实现.js
@@ -4,7 +4,7 @@ Function.prototype.mycall = function(thisArg, ...args) {
      //转换this为一个正确的对象类型 防止其传入的是非对象类型 更要防止传入的this是不是undefined和null，如果是则把这个this绑定到window上
      //这里不能直接写this ? Object(this): this的原因是，如果false,0这样不是null和undefined被当做this值的话，我们的this也会被绑定到window上，我们并不希望这样
      //在node环境下全局为global
-     thisArg = ( this !== null || this !== undefined ) ? Object(thisArg) : typeof window === 'undefined' ? global : window; //number => Number / string => String / boolean => Boolean...
+     thisArg = ( this !== null && this !== undefined ) ? Object(thisArg) : typeof window === 'undefined' ? global : window; //number => Number / string => String / boolean => Boolean...
      thisArg.fn = fn;
      //调用该函数
      let res = thisArg.fn(...args); //又是隐式绑定 this === thisArg
