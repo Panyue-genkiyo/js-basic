31d9612913afc1f7a0504cc5797528a01fef67e3
panyue <90938238g@gmail.com>
2021-11-09 23:14:27 +0800

    weakMap的应用场景(响应式原理)

--- 新增数据结构(map,set,weakSet,weakMap)/05.响应式原理中的weakMap使用.js
+++ 新增数据结构(map,set,weakSet,weakMap)/05.响应式原理中的weakMap使用.js
@@ -1,10 +1,10 @@
 //weakMap使用场景 vue3响应式
-const obj3 = {
+let obj3 = {
     name: 'hello',
     age: 16
 }
 
-const obj4 = {
+let obj4 = {
     address:'shanghai',
     name:'wanky'
 }
@@ -18,6 +18,7 @@ function fn1(){
 const wk = new WeakMap();
 const obj3Map = new Map();
 obj3Map.set('name', [fn1]);
+//注意这里使用weakMap的原因是当我们的obj3被销毁的时候(obj3 = null)，我们希望obj3Map也会被销毁。(弱引用) 但使用map无法达到这一效果
 wk.set(obj3,obj3Map);
 
 //3 当obj3.name发生改变时，找出依赖它的函数并执行
@@ -25,3 +26,7 @@ obj3.name = 'hello world';
 const target = wk.get(obj3);
 const fns = target.get('name');
 fns.forEach(fn => fn());
+
+
+// obj3 = null;
+// console.log(wk.get(obj3)); //undefined;
