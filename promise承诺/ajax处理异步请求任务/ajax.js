2f42ac1e609bdac750196d236738c99022413090
panyue <90938238g@gmail.com>
2021-11-09 23:43:32 +0800

    解决this丢失的的方式

--- promise承诺/ajax处理异步请求任务/ajax.js
+++ promise承诺/ajax处理异步请求任务/ajax.js
@@ -15,7 +15,7 @@ class HttpError extends Error {
 function ajax(url) {
     return new Promise(function(resolve, reject) {
      
-        py.style.display = 'block'; //在发送请求时弹出loading框
+        // py.style.display = 'block'; //在发送请求时弹出loading框
 
         // 创建XMLHttpRequest对象
         if(!/^https?/i.test(url)){
@@ -28,13 +28,15 @@ function ajax(url) {
            if(this.status === 200){
                setTimeout(() => {
                      resolve(JSON.parse(this.response));
-               }, 2000);
+               }, 1000);
+            // resolve(JSON.parse(this.response));
            }else if(this.status === 404){
             //    reject('加载失败');
             //  throw new HttpError('加载失败'); //这里抛出错误是不行的，因为这个错误是异步的，而不是同步执行的,在异步的回调函数中抛出错误无法被外界的promise的catch到
-              setTimeout(() => {
-                   reject(new HttpError('查询的用户不存在'));
-              });
+            //   setTimeout(() => {
+            //        reject(new HttpError('查询的用户不存在'));
+            //   });
+              reject(new HttpError('查询的用户不存在'));
            }
         }
         xhr.onerror = function(){
