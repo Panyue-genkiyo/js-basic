f1bdaf1dc46687f20d1689e5e17078d9be832e7d
panyue <90938238g@gmail.com>
2021-12-07 16:53:04 +0800

    节流函数添加(leading,trailing),指定节流函数是否要在开始或者最后一次结束延迟之后的执行

--- 防抖节流函数深拷贝/06_throttle-v2-处理开始和尾部是否要执行.js
+++ 防抖节流函数深拷贝/06_throttle-v2-处理开始和尾部是否要执行.js
@@ -6,7 +6,7 @@ function throttle(fn, delay, options = { leading: true, trailing: false }) {
     let timer = null;
     const fnByThrottle = function (...args){
          //当事件触发时记录当前时间
-        const curTime = Date.now()
+        const curTime = Date.now();
         if(!lastTime && !leading) {
             //当你不希望第一次在这里执行节流函数的时候，你可以在第一次触发事件的时候将curTime和lastTime设置为同样的值
             lastTime = curTime;
