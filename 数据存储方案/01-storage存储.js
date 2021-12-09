78a5b4c5e6c31858bfbd3fc64bafadede2d7f075
panyue <90938238g@gmail.com>
2021-12-09 16:26:37 +0800

    storage的使用(localstorage,sessionStorage)

---
+++ 数据存储方案/01-storage存储.js
@@ -0,0 +1,5 @@
+const loginBtn = document.querySelector('button');
+loginBtn.onclick = function (){
+    localStorage.setItem('name','localStorage');
+    sessionStorage.setItem('name', 'sessionStorage');
+}
