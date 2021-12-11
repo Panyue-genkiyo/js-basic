030150e6e2a5ce46dff557bd8001a96a8bf723da
panyue <90938238g@gmail.com>
2021-12-11 15:57:25 +0800

    commonjs初次基本使用

---
+++ js模块化开发/commonjs模块化/01-基本使用/main.js
@@ -0,0 +1,5 @@
+//导入模块
+const { name, age, sum} = require('./py.js'); //后缀名省略,js => json => .node
+
+console.log(name, age, sum(2,4));
+
---
+++ js模块化开发/commonjs模块化/01-基本使用/py.js
@@ -0,0 +1,14 @@
+const name = 'py';
+const age = 21;
+
+function sum(n1, n2){
+    return n1 + n2;
+}
+
+
+//导出
+module.exports = {
+    name,
+    age,
+    sum
+}
